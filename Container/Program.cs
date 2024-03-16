4d221f682fc45137f3400aa8df287ac235aab7d7
FupsGamer <33037022+FupsGamer@users.noreply.github.com>
2024-03-16 14:54:12 +0100

    now it properly adds the lists  - program.cs
    now it properly adds the lists for each column in the excel file! also now the filepaty is local to the file

--- Container/Program.cs
+++ Container/Program.cs
@@ -1,54 +1,60 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data; 
 using System.IO;
 using ExcelDataReader; 
+using System;
 using System.Text;
-
-namespace ConsoleApp
+public class ExcelReader
 {
-    public class ExcelReader
+    public static List<string> ImportColumn(string filePath, int columnIndex, int rowIndex)
     {
-        public static List<string> ImportColumn(string filePath, int columnIndex)
+        List<string> columnData = new List<string>();
+
+        using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
         {
-            var columnData = new List<string>();
-
-            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            using (var reader = ExcelReaderFactory.CreateReader(stream))
             {
-                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                var result = reader.AsDataSet(new ExcelDataSetConfiguration()
                 {
-                    var result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
                     {
-                        ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
-                        {
-                            UseHeaderRow = true // Set this to false if your column does not have a header
-                        }
-                    });
+                        UseHeaderRow = false // Assuming the header is not to be used....
+                    }
+                });
 
-                    var dataTable = result.Tables[0];
-                    foreach (DataRow row in dataTable.Rows)
+                var dataTable = result.Tables[0];
+                for (int i = rowIndex; i < dataTable.Rows.Count; i++)
+                {
+                    DataRow row = dataTable.Rows[i];
+                    if (row[columnIndex] != DBNull.Value)
                     {
                         columnData.Add(row[columnIndex].ToString());
                     }
                 }
             }
-
-            return columnData;
         }
-    }
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-            var columnData = ExcelReader.ImportColumn(@"/Users/lajoskariko/RiderProjects/Semester-Project/Container/Data/2024 Heat Production Optimization - Danfoss Deliveries - Source Data Manager.xlsx", 4);
-            foreach (var cellValue in columnData)
-            {
-                Console.WriteLine(cellValue);
-            }
-        }
+        return columnData;
     }
 }
 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        // Guys! Adjust the columnIndex and rowIndex as needed
+        var winter_Time_from = ExcelReader.ImportColumn(@"2024 Heat Production Optimization - Danfoss Deliveries - Source Data Manager.xlsx", 2, 3);
+        var winter_Time_to = ExcelReader.ImportColumn(@"2024 Heat Production Optimization - Danfoss Deliveries - Source Data Manager.xlsx", 3, 3);
+        var winter_HeatDemand = ExcelReader.ImportColumn(@"2024 Heat Production Optimization - Danfoss Deliveries - Source Data Manager.xlsx", 4, 3);
+        var winter_ElectricityPrice = ExcelReader.ImportColumn(@"2024 Heat Production Optimization - Danfoss Deliveries - Source Data Manager.xlsx", 5, 3);
+
+        foreach (var cellValue in winter_Time_from)
+        {
+            Console.WriteLine(cellValue);
+        }
+    }
+}
