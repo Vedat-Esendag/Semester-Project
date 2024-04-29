70a0e0e024958605883b054352ff6f157eb00c81
lajoskariko <lajoskariko03@gmail.com>
2024-04-29 12:13:49 +0200

    .

--- Result_Data_Manager/obj/Debug/net8.0/Result_Data_Manager.AssemblyInfo.cs
+++ Result_Data_Manager/obj/Debug/net8.0/Result_Data_Manager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Result_Data_Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9344a9fdbe871e60b23b996f54b4326961f0db3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ce628c4540d04cabefdc3969c1f888e48c868b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Result_Data_Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Result_Data_Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
