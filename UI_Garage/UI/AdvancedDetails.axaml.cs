1c146691afb1693ccb0051cd2dede7cfed6fbf2b
lajoskariko <lajoskariko03@gmail.com>
2024-05-03 13:29:12 +0200

    buttons

--- UI_Garage/UI/AdvancedDetails.axaml.cs
+++ UI_Garage/UI/AdvancedDetails.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace UI;
@@ -14,5 +15,35 @@ public partial class AdvancedDetails : UserControl
     private void InitializeComponent()
     {
         AvaloniaXamlLoader.Load(this);
+    }
+    
+    private void NavigateToPuPage_Click(object sender, RoutedEventArgs e)
+    {
+        var puPage = new PuPage();
+        Content = puPage;
+    }
+
+    private void NavigateToHeatDemand_Click(object sender, RoutedEventArgs e)
+    {
+        var heatPage = new HeatPage();
+        Content = heatPage;
+    }
+        
+    private void NavigateToElectricityPrice_Click(object sender, RoutedEventArgs e)
+    {
+        var electricityPage = new ElectricityPrice();
+        Content = electricityPage;
+    }
+        
+    private void NavigateToConsumption_Click(object sender, RoutedEventArgs e)
+    {
+        var consumption = new Consumption();
+        Content = consumption;
+    }
+        
+    private void NavigateToAdvancedDetails_Click(object sender, RoutedEventArgs e)
+    {
+        var advancedDetails = new AdvancedDetails();
+        Content = advancedDetails;
     }
 }
--- UI_Garage/UI/HeatDemand.axaml.cs
+++ UI_Garage/UI/HeatDemand.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace UI
@@ -13,6 +14,30 @@ namespace UI
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+        
+        private void NavigateToPuPage_Click(object sender, RoutedEventArgs e)
+        {
+            var puPage = new PuPage();
+            Content = puPage;
+        }
+        
+        private void NavigateToElectricityPrice_Click(object sender, RoutedEventArgs e)
+        {
+            var electricityPage = new ElectricityPrice();
+            Content = electricityPage;
+        }
+        
+        private void NavigateToConsumption_Click(object sender, RoutedEventArgs e)
+        {
+            var consumption = new Consumption();
+            Content = consumption;
+        }
+        
+        private void NavigateToAdvancedDetails_Click(object sender, RoutedEventArgs e)
+        {
+            var advancedDetails = new AdvancedDetails();
+            Content = advancedDetails;
         }
     }
 }
