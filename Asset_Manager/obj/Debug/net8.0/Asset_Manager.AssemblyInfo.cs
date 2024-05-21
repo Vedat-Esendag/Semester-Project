8545e2d4a1ee9c4f8e6b5c779be8b454812d0c17
lajoskariko <lajoskariko03@gmail.com>
2024-05-21 13:43:47 +0200

    making the UI prettier

--- Asset_Manager/obj/Debug/net8.0/Asset_Manager.AssemblyInfo.cs
+++ Asset_Manager/obj/Debug/net8.0/Asset_Manager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asset_Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32b3daf747fa8c11b1bc31b11d2aa670f88364b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94908dd13859d7591fce1d269dc5173c7a1b51e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asset_Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asset_Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
