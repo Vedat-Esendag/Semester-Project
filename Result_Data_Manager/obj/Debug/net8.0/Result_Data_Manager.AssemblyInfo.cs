23a2e030796e822e26611aae4ba9cf05fe598c5e
Vedat-Esendag <91906898+Vedat-Esendag@users.noreply.github.com>
2024-05-26 18:58:43 +0300

    still not working
    :(

--- Result_Data_Manager/obj/Debug/net8.0/Result_Data_Manager.AssemblyInfo.cs
+++ Result_Data_Manager/obj/Debug/net8.0/Result_Data_Manager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Result_Data_Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58781c4ea491c03bee845b7f58133151be4fd06c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+598c5408640568a931d762d97118a5cfa33492f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Result_Data_Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Result_Data_Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
